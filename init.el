6d1bc94acdc717f3c154c83d6f034bed8efe544b
jasonm23 <jasonm23@gmail.com>
2025-06-29 09:50:34 +0700

    High speed git commit invocation of Emacs from cli

--- init.el
+++ init.el
@@ -18,45 +18,51 @@
     ;; xterm-mouse-mode in terminal
     (xterm-mouse-mode 1)))
 
-;; Straight
-(defvar bootstrap-version)
+;;; Exit early if in commit mode
+(add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . diff-mode))
 
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(unless (seq-find
+	 #'(lambda (a) (string-match-p "COMMIT_EDITMSG" a 0))
+	 command-line-args)
 
-;; Require use-package
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+  ;; Straight
+  (defvar bootstrap-version)
 
-;; Straight enabled package config
-(load (file-name-concat user-emacs-directory "packages.el"))
-;; Load everything from /use
-(mapc
- 'load
- (file-expand-wildcards
-  (file-name-concat user-emacs-directory "use/*.el")))
+  (let ((bootstrap-file
+	 (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+	(bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
-;; Load creamsody, make it darker
-(load-theme 'creamsody 1)
-(load-theme 'creamsody-darker 1)
+  ;; Require use-package
+  (straight-use-package 'use-package)
+  (setq straight-use-package-by-default t)
 
-(let ((local-emacs-conf "~/.config/emacs-local.el")
-      (local-emacs-custom "~/.config/emacs-custom.el"))
-  (setq custom-file local-emacs-custom)
-  (when (file-exists-p custom-file)
-    (load custom-file))
-  (when (file-exists-p local-emacs-conf)
-    (load local-emacs-conf)))
+  ;; Straight enabled package config
+  (load (file-name-concat user-emacs-directory "packages.el"))
+  ;; Load everything from /use
+  (mapc
+   'load
+   (file-expand-wildcards
+    (file-name-concat user-emacs-directory "use/*.el")))
 
+  ;; Load creamsody, make it darker
+  (load-theme 'creamsody 1)
+  (load-theme 'creamsody-darker 1)
+
+  (let ((local-emacs-conf "~/.config/emacs-local.el")
+	(local-emacs-custom "~/.config/emacs-custom.el"))
+    (setq custom-file local-emacs-custom)
+    (when (file-exists-p custom-file)
+      (load custom-file))
+    (when (file-exists-p local-emacs-conf)
+      (load local-emacs-conf))))
