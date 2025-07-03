aaefd0632e2c53572eae0154d4b133ecab211d30
jasonm23 <jasonm23@gmail.com>
2025-07-03 23:05:50 +0700

    Turn off treesit-auto by default...

--- use/use-treesit-auto.el
+++ use/use-treesit-auto.el
@@ -1,6 +1,11 @@
-(use-package treesit-auto
-  :custom
-  (treesit-auto-install t)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+;;(use-package treesit-auto
+  ;;:custom
+  ;;(treesit-auto-install t)
+  ;;:config
+  ;;(treesit-auto-add-to-auto-mode-alist 'all)
+;;(global-treesit-auto-mode))
+
+;;; Great but very annoying when it tries to setup for bash fails,
+;;; overrides sh-mode and ... in general makes a mess of things.
+;;  M-x global-treesit-auto-mode is enough to get going until the bash
+;;  issue gets some time to sort out...
