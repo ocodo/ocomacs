548fe94ec4ba0bc9c2d3c71b4985027c50546881
jasonm23 <jasonm23@gmail.com>
2025-07-06 07:44:29 +0700

    Add git-timemachine, diff-hl and organize packages.el

--- packages.el
+++ packages.el
@@ -1,31 +1,45 @@
 ;; Pacakages to install
-(straight-use-package 'creamsody-theme)
-(straight-use-package 'emacs-dashboard)
-(straight-use-package 'editorconfig)
+;; Core
 (straight-use-package 's)
-(straight-use-package 'move-text)
+(straight-use-package 'emacs-dashboard)
 (straight-use-package 'which-key)
+(straight-use-package 'editorconfig)
+(straight-use-package 'move-text)
+(straight-use-package 'iedit)
+(straight-use-package 'company)
 (straight-use-package 'lsp-mode)
 (straight-use-package 'vertico)
 (straight-use-package 'orderless)
 (straight-use-package 'marginalia)
-(straight-use-package 'ellama)
+
+;; Git
 (straight-use-package 'magit)
-(straight-use-package 'kurecolor)
+(straight-use-package 'git-timemachine)
+(straight-use-package 'diff-hl)
+
+;; Additional Languages
 (straight-use-package 'markdown-mode)
-(straight-use-package 'jtsx)
-(straight-use-package 'company)
 (straight-use-package 'anaconda-mode)
 (straight-use-package 'lua-mode)
-(straight-use-package 'iedit)
+;; Add more langs via ~/.config/ocomacs/
+
+;; See ./use/use-treesit-auto.el for config
+(straight-use-package 'treesit-auto)
+
+;; Snippets
 (straight-use-package 'yasnippet)
 (straight-use-package 'yasnippet-snippets)
-(straight-use-package 'treesit-auto)
-(straight-use-package 'editorconfig)
+;; Ollama LLM access
+(straight-use-package 'ellama)
+;; Potentiall dropped - Move to ~/.config/ocomacs/
 
+;; Presentation Core
 (straight-use-package 'all-the-icons)
 (straight-use-package
  '(lambda-line
    :type git
    :host github
    :repo "Lambda-Emacs/lambda-line"))
+
+;; Default Theme (creamsody, creamsody-darker)
+(straight-use-package 'creamsody-theme)
