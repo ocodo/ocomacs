3a21fce5d7f59fc98e37fa730767aed3107e1f64
jasonm23 <jasonm23@gmail.com>
2025-07-06 07:38:32 +0700

    Better treesit-auto support

--- use/use-treesit-auto.el
+++ use/use-treesit-auto.el
@@ -1,11 +1,14 @@
-;;(use-package treesit-auto
-  ;;:custom
-  ;;(treesit-auto-install t)
-  ;;:config
-  ;;(treesit-auto-add-to-auto-mode-alist 'all)
-;;(global-treesit-auto-mode))
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install t)
 
-;;; Great but very annoying when it tries to setup for bash fails,
-;;; overrides sh-mode and ... in general makes a mess of things.
-;;  M-x global-treesit-auto-mode is enough to get going until the bash
-;;  issue gets some time to sort out...
+  :config
+  (setq
+   treesit-auto-langs
+   '(json markdown python tsx typescript javascript go yaml))
+
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+;; The full treesit-auto-langs for ref
+;; '(awk bibtex blueprint c c-sharp clojure cmake commonlisp cpp css dart dockerfile elixir glsl go gomod heex html janet java javascript json julia kotlin latex lua magik make markdown nix nu org perl proto python r ruby rust scala sql surface toml tsx typescript typst verilog vhdl vue wast wat wgsl yaml))
